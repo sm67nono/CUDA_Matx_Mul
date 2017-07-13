ebf32dd810bb5d4dc6bd7883f87416b26b05f604
sm67nono <manna.subhadeep@gmail.com>
2017-07-14 01:45:48 +0200

    Update mat_mul_test_kernal.cu

--- mat_mul_test_kernal.cu
+++ mat_mul_test_kernal.cu
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <chrono>
+#include <memory>
 using namespace std;
 using namespace std::chrono;
 
@@ -12,57 +13,62 @@ cudaError_t performNormalMatrixMultiplication();
 cudaError_t performJacobi();
 
 
-/*__device__ __forceinline__ float multBandedMatrixVectorRow(
-	const float *A0, const float *A1, const float *A2, const float *A3, const float *A4, const float *x,
-	int idx, int2 pos, int2 dim)
+struct cuda_deleter
 {
-	float res = 0.f;
+	void operator() (void * p) { cudaFree(p); }
+};
 
-
-	res += A2[idx] * x[idx];
-
-	// left, right
-	if (pos.x > 0)
-		res += A1[idx] * x[idx - 1];
-	if (pos.x < dim.x - 1)
-		res += A3[idx] * x[idx + 1];
-
-	// up, down
-	if (pos.y > 0)
-		res += A0[idx] * x[idx - dim.x];
-	if (pos.y < dim.y - 1)
-		res += A4[idx] * x[idx + dim.x];
-	//#endif
-	return res;
+template<typename T>
+auto make_unique_cuda_array(std::size_t size)
+{
+	T * p = nullptr;
+	if (auto err = cudaMalloc((void**)&p, size * sizeof(T)))
+		throw std::bad_alloc();
+	return std::unique_ptr<T[], cuda_deleter>(p);
 }
 
 
 
-
+/*__device__ __forceinline__ float multBandedMatrixVectorRow(
+const float *A0, const float *A1, const float *A2, const float *A3, const float *A4, const float *x,
+int idx, int2 pos, int2 dim)
+{
+float res = 0.f;
+res += A2[idx] * x[idx];
+// left, right
+if (pos.x > 0)
+res += A1[idx] * x[idx - 1];
+if (pos.x < dim.x - 1)
+res += A3[idx] * x[idx + 1];
+// up, down
+if (pos.y > 0)
+res += A0[idx] * x[idx - dim.x];
+if (pos.y < dim.y - 1)
+res += A4[idx] * x[idx + dim.x];
+//#endif
+return res;
+}
 //Single GPU Kernel for Jacobi
 /* template<bool addResult, typename Real>
 __global__ void multMatrix_kernel(const float *A0, const float *A1, const float *A2, const float *A3, const float *A4, int2 dim, const Real * x, Real * y)
 {
-	int2 pos;
-
-	pos.x = IMUL(blockIdx.x, blockDim.x) + threadIdx.x;
-	pos.y = IMUL(blockIdx.y, blockDim.y) + threadIdx.y;
-
-	int idx = pos.x + IMUL(pos.y, dim.x);
-	//int tdx = IMUL(threadIdx.y, blockDim.x) + threadIdx.x;
-
-	if (pos.x > 0 && pos.x < dim.x - 1 && pos.y > 0 && pos.y < dim.y - 1)
-	{
-		Real sum = multBandedMatrixVectorRow(A0, A1, A2, A3, A4, x, idx, pos, dim);
-		if (addResult)
-		{
-			y[idx] += sum;
-		}
-		else
-		{
-			y[idx] = sum;
-		}
-	}
+int2 pos;
+pos.x = IMUL(blockIdx.x, blockDim.x) + threadIdx.x;
+pos.y = IMUL(blockIdx.y, blockDim.y) + threadIdx.y;
+int idx = pos.x + IMUL(pos.y, dim.x);
+//int tdx = IMUL(threadIdx.y, blockDim.x) + threadIdx.x;
+if (pos.x > 0 && pos.x < dim.x - 1 && pos.y > 0 && pos.y < dim.y - 1)
+{
+Real sum = multBandedMatrixVectorRow(A0, A1, A2, A3, A4, x, idx, pos, dim);
+if (addResult)
+{
+y[idx] += sum;
+}
+else
+{
+y[idx] = sum;
+}
+}
 } */
 
 
@@ -85,7 +91,7 @@ __global__ void jacobi_Simple(const float *A0, const float *A1, const float *A2,
 	//Carry out computations for boundary elements
 	if (index == leftBoundaryElem && index == bottomBoundaryElem) // Bottom left Corner Element
 	{
-		
+
 		//Top
 		result -= A4[index] * x[index + dim];
 
@@ -140,7 +146,7 @@ __global__ void jacobi_Simple(const float *A0, const float *A1, const float *A2,
 	{
 		//Bottom
 		result -= A0[index] * x[index - dim];
-		
+
 		//Left
 		result -= A1[index] * x[index - 1];
 
@@ -152,7 +158,7 @@ __global__ void jacobi_Simple(const float *A0, const float *A1, const float *A2,
 
 	}
 
-	
+
 
 
 	else if (index == leftBoundaryElem)
@@ -169,7 +175,7 @@ __global__ void jacobi_Simple(const float *A0, const float *A1, const float *A2,
 		result -= A3[index] * x[index + 1];
 
 		result /= A2[index];
-		
+
 		x[index] = result;
 
 		return;
@@ -242,7 +248,7 @@ __global__ void jacobi_Simple(const float *A0, const float *A1, const float *A2,
 
 
 	}
-		
+
 	//For every other element not on the boundary
 	else {
 		//Bottom
@@ -264,7 +270,7 @@ __global__ void jacobi_Simple(const float *A0, const float *A1, const float *A2,
 
 		return;
 	}
-	 
+
 }
 
 
@@ -301,33 +307,33 @@ void initDiag(float *A0, float *A1, float *A2, float *A3, float *A4, float *res,
 {
 	//Not accounted for Obstacles
 
-	for (unsigned int i = 0; i < dim; ++i)
+	for (int i = 0; i < dim; ++i)
 	{
-		for (unsigned int j = 0; j < dim; ++j)
+		for (int j = 0; j < dim; ++j)
 		{
-			int idx = j * dim + i;
+			int idx = j + dim * i;
 
 			//Bottom
-			if (i==0) {
+			if (i == 0) {
 				A0[idx] = 0.0f;
 			}
-			else{
+			else {
 				A0[idx] = 1.0f;
 			}
 
 			//Left 
-			if(j==0)
+			if (j == 0)
 			{
 				A1[idx] = 0.0f;
 			}
 			else {
-			
+
 				A1[idx] = 1.0f;
 			}
-			
+
 
 			//Right
-			if (j == dim-1)
+			if (j == dim - 1)
 			{
 				A3[idx] = 0.0f;
 			}
@@ -350,8 +356,9 @@ void initDiag(float *A0, float *A1, float *A2, float *A3, float *A4, float *res,
 			A2[idx] = 1.0f;
 
 			//Result(RHS) and Vector init
-			res[idx] = 0.0 + i * 0.15;
+			res[idx] = 2.0+j*i/0.78;
 			vec[idx] = 0.0f;
+			
 
 		}
 	}
@@ -360,52 +367,47 @@ void initDiag(float *A0, float *A1, float *A2, float *A3, float *A4, float *res,
 
 
 
-#include <memory>
-
-struct cuda_deleter
-{
-	void operator() (void * p) { cudaFree(p); }
-};
-
-template<typename T>
-auto make_unique_cuda_array(std::size_t size)
-{
-	T * p = nullptr;
-	if(auto err = cudaMalloc((void**)&p, size * sizeof(T)))
-		throw std::bad_alloc();
-	return std::unique_ptr<T[], cuda_deleter>(p);
-}
 
 cudaError_t performJacobi()
 {
 
 
 	//Fixed values to be changed later
-	const int size = 1024;
 
-	const int dim = 32;
+	const int dim = 5;
+
+	const int size = dim * dim;
+
 	auto result = std::make_unique<float[]>(size);
 
 	//Create Diagonal Vectors
-	auto a0 = std::make_unique<float[]>(size);//To copy final result from device to host
-	auto a1 = std::make_unique<float[]>(size); 
-	auto a2 = std::make_unique<float[]>(size); 
-	auto a3 = std::make_unique<float[]>(size); 
+	auto a0 = std::make_unique<float[]>(size);
+	auto a1 = std::make_unique<float[]>(size);
+	auto a2 = std::make_unique<float[]>(size);
+	auto a3 = std::make_unique<float[]>(size);
 	auto a4 = std::make_unique<float[]>(size);
-	auto d_Vec = std::make_unique<float[]>(size);
-	auto d_Res = std::make_unique<float[]>(size);
+	auto vec = std::make_unique<float[]>(size);
+	auto res = std::make_unique<float[]>(size);
 
 
-	initDiag(a0.get(), a1.get(), a2.get(), a3.get(), a4.get(), d_Vec.get(), d_Res.get(), dim);
+	initDiag(a0.get(), a1.get(), a2.get(), a3.get(), a4.get(), res.get(), vec.get(), dim);
 
 
+	
 	//For use on Device 
 	auto d_A0 = make_unique_cuda_array<float>(size);
 	auto d_A1 = make_unique_cuda_array<float>(size);
 	auto d_A2 = make_unique_cuda_array<float>(size);
 	auto d_A3 = make_unique_cuda_array<float>(size);
 	auto d_A4 = make_unique_cuda_array<float>(size);
+	auto d_Vec = make_unique_cuda_array<float>(size);
+	auto d_Res = make_unique_cuda_array<float>(size);
 
+	if (auto err = cudaGetLastError())
+	{
+		fprintf(stderr, "Jacobi launch failed: %s\n", cudaGetErrorString(err));
+		return err;
+	}
 
 	//cudamalloc the Diagonals
 	cudaMalloc((void**)&d_A0, size * sizeof(float));
@@ -413,11 +415,17 @@ cudaError_t performJacobi()
 	cudaMalloc((void**)&d_A2, size * sizeof(float));
 	cudaMalloc((void**)&d_A3, size * sizeof(float));
 	cudaMalloc((void**)&d_A4, size * sizeof(float));
-							
+
 	//cudamalloc the Input Vector and Result vector
-	cudaMalloc((void**)&d_Vec, size  * sizeof(float));
-	cudaMalloc((void**)&d_Res, size  * sizeof(float));
-							   
+	cudaMalloc((void**)&d_Vec, size * sizeof(float));
+	cudaMalloc((void**)&d_Res, size * sizeof(float));
+
+	if (auto err = cudaGetLastError())
+	{
+		fprintf(stderr, "Jacobi launch failed: %s\n", cudaGetErrorString(err));
+		return err;
+	}
+
 
 	cudaMemcpy(d_A0.get(), a0.get(), size * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_A1.get(), a1.get(), size * sizeof(float), cudaMemcpyHostToDevice);
@@ -425,21 +433,31 @@ cudaError_t performJacobi()
 	cudaMemcpy(d_A3.get(), a3.get(), size * sizeof(float), cudaMemcpyHostToDevice);
 	cudaMemcpy(d_A4.get(), a4.get(), size * sizeof(float), cudaMemcpyHostToDevice);
 
-	cudaMemcpy(d_A4.get(), d_Vec.get(), size * sizeof(float), cudaMemcpyHostToDevice);
-	cudaMemcpy(d_A4.get(), d_Res.get(), size * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_Vec.get(), vec.get(), size * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_Res.get(), res.get(), size * sizeof(float), cudaMemcpyHostToDevice);
 
+	if (auto err = cudaGetLastError())
+	{
+		fprintf(stderr, "Jacobi launch failed: %s\n", cudaGetErrorString(err));
+		return err;
+	}
 
 	//multMatrix(d_A0, d_A1, d_A2, d_A3, d_A4, myDim, d_vec, d_res);
 
 	//Perform one Jacobi Step
-	int blocksize = 32;
-	int threads = 32;
+	int blocksize = 5;
+	int threads = 5;
 
 
-	jacobi_Simple<<<blocksize,threads>>>(d_A0.get(), d_A1.get(), d_A2.get(), d_A3.get(), d_A4.get(), dim, d_Vec.get(), d_Res.get());
+	jacobi_Simple <<<blocksize, threads >>>(d_A0.get(), d_A1.get(), d_A2.get(), d_A3.get(), d_A4.get(), dim, d_Vec.get(), d_Res.get());
 
+	if (auto err = cudaGetLastError())
+	{
+		fprintf(stderr, "Jacobi launch failed: %s\n", cudaGetErrorString(err));
+		return err;
+	}
 
-	cudaMemcpy(result.get(), d_Res.get(), size* sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(result.get(), d_Vec.get(), size * sizeof(float), cudaMemcpyDeviceToHost);
 
 	if (auto err = cudaGetLastError())
 	{
@@ -449,6 +467,23 @@ cudaError_t performJacobi()
 
 
 	cout << "One iteration successful";
+
+	//Print result
+	for (int i = 0; i < size; i++) {
+
+
+		if (i % dim == 0) { cout << endl; }
+
+		cout << vec[i] << " ";
+	}
+
+	for (int i = 0; i < size; i++) {
+	
+		
+		if (i % dim == 0) { cout << endl; }
+
+		cout << result[i] << " ";
+	}
 	// Freeing memory auto done by cuda deleter
 
 	/*cudaFree(d_A0.get());
@@ -456,7 +491,6 @@ cudaError_t performJacobi()
 	cudaFree(d_A2.get());
 	cudaFree(d_A3.get());
 	cudaFree(d_A4.get());
-
 	cudaFree(d_Vec.get());
 	cudaFree(d_Res.get());*/
 
@@ -473,14 +507,13 @@ cudaError_t performNormalMatrixMultiplication()
 	auto x = std::make_unique<float[]>(size); //Vector to be multiplied
 	auto b = std::make_unique<float[]>(size); //Resultant vector
 
-	
+
 
 	initArrays(a.get(), x.get(), b.get(), size);
 
-	//testInitilization(a, x, b);
-
+	
 	// Choose which GPU to run on, change this on a multi-GPU system.
-	if(auto err = cudaSetDevice(0))
+	if (auto err = cudaSetDevice(0))
 	{
 		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
 		return err;
@@ -493,14 +526,14 @@ cudaError_t performNormalMatrixMultiplication()
 
 	// Copy input vectors from host memory to GPU buffers.
 	cout << "memcopy 1 \n";
-	if(auto err = cudaMemcpy(dev_a.get(), a.get(), size * size * sizeof(float), cudaMemcpyHostToDevice))
+	if (auto err = cudaMemcpy(dev_a.get(), a.get(), size * size * sizeof(float), cudaMemcpyHostToDevice))
 	{
 		fprintf(stderr, "cudaMemcpy failed!");
 		return err;
 	}
 
 	cout << "memcopy 2 \n";
-	if(auto err = cudaMemcpy(dev_x.get(), x.get(), size * sizeof(float), cudaMemcpyHostToDevice))
+	if (auto err = cudaMemcpy(dev_x.get(), x.get(), size * sizeof(float), cudaMemcpyHostToDevice))
 	{
 		fprintf(stderr, "cudaMemcpy failed!");
 		return err;
@@ -512,10 +545,10 @@ cudaError_t performNormalMatrixMultiplication()
 	int blocksize = 8;
 	printf("The BlockSize is %d \n", blocksize);
 	high_resolution_clock::time_point t1 = high_resolution_clock::now();
-	multiply<<<blocksize, threads>>>(dev_a.get(), dev_x.get(), dev_b.get(), size);
+	multiply << <blocksize, threads >> >(dev_a.get(), dev_x.get(), dev_b.get(), size);
 
 	// Check for any errors launching the kernel
-	if(auto err = cudaGetLastError())
+	if (auto err = cudaGetLastError())
 	{
 		fprintf(stderr, "multiply launch failed: %s\n", cudaGetErrorString(err));
 		return err;
@@ -524,7 +557,7 @@ cudaError_t performNormalMatrixMultiplication()
 
 	// cudaDeviceSynchronize waits for the kernel to finish, and returns
 	// any errors encountered during the launch.
-	if(auto err = cudaDeviceSynchronize())
+	if (auto err = cudaDeviceSynchronize())
 	{
 		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", err);
 		return err;
@@ -542,12 +575,9 @@ cudaError_t performNormalMatrixMultiplication()
 	// Copy output vector from GPU buffer to host memory.
 	cout << "memcopy 3 \n";
 	/*cudaStatus = cudaMemcpy(c, dev_b, 32 * sizeof(float), cudaMemcpyDeviceToHost);
-
-
-
 	if (cudaStatus != cudaSuccess) {
-		fprintf(stderr, "cudaMemcpy failed! \n");
-		goto Error;
+	fprintf(stderr, "cudaMemcpy failed! \n");
+	goto Error;
 	}*/
 
 	cout << "all memcopy succeed \n";
